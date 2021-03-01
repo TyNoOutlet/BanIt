f8dce1cd098b0d87ff0564515414d09e721f6aaa
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2021-03-01 19:37:14 +0200

    Update MainModule.lua
    Made the code much cleaner, made it more compact and better optimised as well as fixed a few bugs.

--- MainModule.lua
+++ MainModule.lua
@@ -4,187 +4,177 @@
 -- More info: https://devforum.roblox.com/t/banit-simple-ban-module-for-anyone/1074218
 -- Version: 7
 
+-- // OUTPUT PREFIX
+local OldPrint, OldWarn = print, warn
+local print, warn = function(...)
+	OldPrint("BanIt | ", ...)
+end, function(...)
+	OldWarn("BanIt-Warning | ", ...)
+end
+
 -- // VARIABLES
 
-local serverBanTable = {}
+local ServerBans = {}
 
 local Players = game:GetService("Players")
-local DSS = game:GetService("DataStoreService")
-local MS = game:GetService("MessagingService")
-local banStore = DSS:GetDataStore("BanStore" .. game.PlaceId .. "123456789")
-local timedBanStore = DSS:GetDataStore("TimedBanStore" .. game.PlaceId .. "123456789")
-local data = nil
-local data2 = nil
+local DataStoreService = game:GetService("DataStoreService")
+local MessagingService = game:GetService("MessagingService")
+local BanStore = DataStoreService:GetDataStore(string.format("BanStore%d123456789", game.PlaceId))
+local TimeBanStore = DataStoreService:GetDataStore(string.format("TimedBanStore%d123456789", game.PlaceId))
+local GlobalBans, TimeBans
 
-local success, data = pcall(function()
-	return banStore:GetAsync("Bans")
+xpcall(function()
+	GlobalBans = BanStore:GetAsync("Bans") or {}
+end, function(Err)
+	warn("DataStore failed. Try turning on Studio Access to API Services. Reason: ", Err)
 end)
 
-if success and not data then
-	data = {}
-elseif not success then
-	warn("DataStore failed. Try turning on Studio Access to API Services.")
-end
-
-local succ, err = pcall(function()
-	data2 = timedBanStore:GetAsync("TimedBans")
+xpcall(function()
+	TimeBans = TimeBanStore:GetAsync("TimedBans") or {}
+end, function(Err)
+	warn("DataStore failed. Try turning on Studio Access to API Services. Reason: ", Err)
 end)
-
-if succ and data2 == nil then
-	data2 = {}
-	print("There was no data")
-elseif not succ then
-	warn("DataStore failed. Try turning on Studio Access to API Services.")
-elseif succ and err then
-	warn(err)
-end
 
 -- // FUNCTIONS
 
 local function saveData()
-	local suc, err = pcall(function()
-		return banStore:SetAsync("Bans", data)
+	xpcall(function()
+		BanStore:SetAsync("Bans", GlobalBans)
+		print("Successfully saved")
+	end, function(Err)
+		warn(Err)
 	end)
-	if not suc and err then
-		warn(err)
-	elseif suc then
-		print("BanIt | Successfully saved")
-	end
 end
 
 local function saveTimeData()
-	local yes, no = pcall(function()
-		return timedBanStore:SetAsync("TimedBans", data2)
-	end)
-	if not yes and no then
-		warn(no)
-	elseif yes then
-		print("BanIt | Successfully saved")
-		for k, v in pairs(data2) do
-			print(k, v)
+	xpcall(function()
+		TimeBanStore:SetAsync("TimedBans", TimeBans)
+		print("Successfully saved")
+		for i, v in pairs(TimeBans) do
+			print(i, v)
 		end
-	elseif not yes then
-		print("idk")
-	end
+	end, function(Err)
+		warn(Err)
+	end)
 end
 
-Players.PlayerAdded:Connect(function(plr)
-	if table.find(data, plr.UserId) or table.find(serverBanTable, plr.UserId) then
-		plr:Kick("Banned from the game!")
-	elseif data2[tostring(plr.UserId)] ~= nil then
-		local strTable = string.split(data2[tostring(plr.UserId)], ";")
-		local timeLeft = os.time() - tonumber(strTable[1])
-		local num = tonumber(strTable[2]) 
-		if num - timeLeft >= 1 then
-			plr:Kick(tostring(num - timeLeft) .. " seconds left on ban.")
+Players.PlayerAdded:Connect(function(Plr)
+	if table.find(GlobalBans, Plr.UserId) or table.find(ServerBans, Plr.UserId) then
+		Plr:Kick("Banned from the game!")
+	elseif TimeBans[tostring(Plr.UserId)] ~= nil then
+		local BanData = string.split(TimeBans[tostring(Plr.UserId)], ";")
+		local TimeLeft = os.time() - tonumber(BanData[1])
+		local BanLenght = tonumber(BanData[2]) 
+		
+		if BanLenght - TimeLeft >= 1 then
+			Plr:Kick(tostring(BanLenght - TimeLeft) .. " seconds left on ban.")
 		end
-	elseif data2[tostring(plr.UserId)] == nil then
-		print("No data for user " .. plr.Name)
+	elseif not TimeBans[tostring(Plr.UserId)] then
+		print("No data for user " .. Plr.Name)
 	end
 end)
 
-local subSuc, subCon = pcall(function()
-	return MS:SubscribeAsync("Ban", function(message)
-		local data = message.Data
-		local dataTable = string.split(data, "⌐")
+xpcall(function()
+	return MessagingService:SubscribeAsync("Ban", function(Message)
+		local dataTable = string.split(Message.Data, "⌐")
 		if Players:FindFirstChild(dataTable[1]) then
 			Players:FindFirstChild(dataTable[1]):Kick(dataTable[2] or "You have been banned.")
 		end
 	end)
-end)
+end, warn)
+
 -- // MODULE
 
 local BanIt = {}
 
-function BanIt.ServerBan(plrUser, reason)
-	local success = pcall(function()
-		plr = Players:GetUserIdFromNameAsync(plrUser)
-	end)
-	if success and plr then
-		table.insert(serverBanTable, plr)
-		if Players:FindFirstChild(plrUser) then
-			Players[plrUser]:Kick(reason or "Banned from the server!")
+function BanIt.ServerBan(PlrInstance, Reason)
+	xpcall(function()
+		local Plr = Players:GetUserIdFromNameAsync(PlrInstance)
+		
+		table.insert(ServerBans, Plr)
+		local FoundPlayer = Players:FindFirstChild(PlrInstance)
+		if FoundPlayer then
+			FoundPlayer:Kick(Reason or "Banned from the server!")
 		end
-	else
-		warn("Player not found in database. Call may have failed, try again.")
-	end
+	end, function(Err)
+		warn("Player not found in database. Call may have failed, try again. Reason: ", Err)
+	end)
 end
 
-function BanIt.Ban(plrUser, reason)
-	local success = pcall(function()
-		plr = Players:GetUserIdFromNameAsync(plrUser)
-	end)
-	if success and plr then
-		table.insert(data, plr)
+function BanIt.Ban(PlrInstance, Reason)
+	xpcall(function()
+		local Plr = Players:GetUserIdFromNameAsync(PlrInstance)
+
+		table.insert(GlobalBans, Plr)
 		saveData()
-		if Players:FindFirstChild(plrUser) then
-			Players[plrUser]:Kick(reason or "Banned from the game!")
+		
+		local FoundPlayer = Players:FindFirstChild(PlrInstance)
+		if FoundPlayer then
+			FoundPlayer:Kick(Reason or "Banned from the game!")
 		else
-			local s, result = pcall(function()
-				return MS:PublishAsync("Ban", plrUser .. "⌐" .. reason)
-			end)
+			xpcall(function()
+				return MessagingService:PublishAsync("Ban", PlrInstance .. "⌐" .. Reason)
+			end, warn)
 		end
-	else
-		warn("Player not found in database. Call may have failed, try again.")
-	end
+	end, function(Err)
+		warn("Player not found in database. Call may have failed, try again. Reason: ", Err)
+	end)
 end
 
-function BanIt.Unban(plrUser)
-	local success = pcall(function()
-		plr = Players:GetUserIdFromNameAsync(plrUser)
-	end)
-	if success and plr then
-		local pos = table.find(data, plr)
-		print(pos)
-		table.remove(data, pos)
-		print(data[pos])
+function BanIt.Unban(PlrInstance)
+	xpcall(function()
+		local Plr = assert(Players:GetUserIdFromNameAsync(PlrInstance), "No player returned by Players:GetUserIdFromNameAsync().")
+
+		local i = table.find(GlobalBans, Plr)
+		
+		print(i)
+		table.remove(GlobalBans, i)
+		print(GlobalBans[i])
 		saveData()
-	else
-		warn("Player not found in database. Call may have failed, try again.")
-	end
+	end, function(Err)
+		warn("Player not found in database. Call may have failed, try again. Reason: ", Err)
+	end)
 end
 
-function BanIt.ServerUnban(plrUser)
-	local success = pcall(function()
-		plr = Players:GetUserIdFromNameAsync(plrUser)
-	end)
-	if success and plr then
-		local pos = table.find(serverBanTable, plr)
-		print(pos)
-		table.remove(serverBanTable, pos)
-		print(serverBanTable[pos])
+function BanIt.ServerUnban(PlrInstance)
+	xpcall(function()
+		local Plr = assert(Players:GetUserIdFromNameAsync(PlrInstance), "No player returned by Players:GetUserIdFromNameAsync().")
+
+		local i = table.find(ServerBans, Plr)
+		
+		print(i)
+		table.remove(ServerBans, i)
+		print(ServerBans[i])
 		saveData()
-	else
-		warn("Player not found in database. Call may have failed, try again.")
-	end
+	end, function(Err)
+		warn("Player not found in database. Call may have failed, try again. Reason: ", Err)
+	end)
 end
 
-function BanIt.TimedBan(plrUser, num, numType)
-	if numType:lower() == "minutes" then
-		num *= 60
-	elseif numType:lower() == "hours" then
-		num *= 3600
-	elseif numType:lower() == "days" then
-		num *= 86400
+function BanIt.TimedBan(PlrInstance, BanLenght, TimeType)
+	if string.lower(TimeType) == "minutes" then
+		BanLenght *= 60
+	elseif string.lower(TimeType) == "hours" then
+		BanLenght *= 3600
+	elseif string.lower(TimeType) == "days" then
+		BanLenght *= 86400
 	end
-	local success = pcall(function()
-		plr = Players:GetUserIdFromNameAsync(plrUser)
-	end)
-	if success and plr then
-		local current = os.time()
-		data2[plr] = current .. ";" .. num
-		print(data2[plr])
+	xpcall(function()
+		local Plr = Players:GetUserIdFromNameAsync(PlrInstance)
+		
+		TimeBans[Plr] = os.time() .. ";" .. BanLenght
+		print(TimeBans[Plr])
 		saveTimeData()
-		if Players:FindFirstChild(plrUser) then
-			Players[plrUser]:Kick("Banned for " .. num .. " " .. numType .. " from the game.")
+		if Players:FindFirstChild(PlrInstance) then
+			Players[PlrInstance]:Kick("Banned for " .. BanLenght .. " " .. TimeType .. " from the game.")
 		else
-			local s, result = pcall(function()
-				return MS:PublishAsync("Ban", plrUser .. "⌐" .. "Banned for " .. num .. " " .. numType .. " from the game.")
-			end)
+			xpcall(function()
+				return MessagingService:PublishAsync("Ban", PlrInstance .. "⌐" .. "Banned for " .. BanLenght .. " " .. TimeType .. " from the game.")
+			end, warn)
 		end
-	else
-		warn("Player not found in database. Call may have failed, try again.")
-	end
+	end, function(Err)
+		warn("Player not found in database. Call may have failed, try again. Reason: ", Err)
+	end)
 end
 
 return BanIt

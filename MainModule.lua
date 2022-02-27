bca28b81e14f3dcb3b02ff97f2368ef542485c77
Ty <74572291+TyNoOutlet@users.noreply.github.com>
2022-02-27 07:55:08 -0500

    Many updates, check update log on DevForum

--- MainModule.lua
+++ MainModule.lua
@@ -2,7 +2,7 @@
 -- Creator: Ty_Scripts
 -- Date created: 2/26/2021 16:00 UTC-5
 -- More info: https://devforum.roblox.com/t/banit-simple-ban-module-for-anyone/1074218
--- Version: 10
+-- Version: 11
 
 -- // VARIABLES
 
@@ -14,8 +14,9 @@ local timedBanStore = DataStoreService:GetDataStore("TimedBanStore123456789")
 local shadowBanStore = DataStoreService:GetDataStore("ShadowBanStore123456789")
 local unbannableStore = DataStoreService:GetDataStore("UnbannableStore1234567890")
 local groupStore = DataStoreService:GetDataStore("GroupStore1234567890")
+local rankStore = DataStoreService:GetDataStore("RankStore1234567890")
 
-local globalBans, timedBans, shadowBans, unbannables, groupBans, serverBanTable = {}, {}, {}, {}, {}, {}
+local globalBans, timedBans, shadowBans, unbannables, groupBans, rankBans, serverBanTable = {}, {}, {}, {}, {}, {}, {}
 
 xpcall(function()
 	globalBans = banStore:GetAsync("Bans") or {}
@@ -43,6 +44,12 @@ end)
 
 xpcall(function()
 	groupBans = groupStore:GetAsync("GroupBans") or {}
+end, function(err)
+	warn("BanIt | DataStore failed. Try turning on Studio Access to API Services. Error: " .. err)
+end)
+
+xpcall(function()
+	rankBans = rankStore:GetAsync("RankBans") or {}
 end, function(err)
 	warn("BanIt | DataStore failed. Try turning on Studio Access to API Services. Error: " .. err)
 end)
@@ -88,7 +95,16 @@ end
 
 local function saveGroupData()
 	xpcall(function()
-		groupStore:SetAsync("Unbannables", unbannables)
+		groupStore:SetAsync("GroupBans", groupBans)
+		print("BanIt | Successfully saved!")
+	end, function(err)
+		warn("BanIt | Data saving failed! Error: " .. err)
+	end)
+end
+
+local function saveRankData()
+	xpcall(function()
+		rankStore:SetAsync("RankBans", rankBans)
 		print("BanIt | Successfully saved!")
 	end, function(err)
 		warn("BanIt | Data saving failed! Error: " .. err)
@@ -96,8 +112,8 @@ local function saveGroupData()
 end
 
 local shadowBanMessages = {
-	".ROBLOXWALKSPEEDJUMPPOWER failure. Please rejoin.\nIncident ticket: 0x4F5A3C4", "ACLI: Loading Error [Took Too Long (>10 Minutes)]",
-	"Loading Error: PlayerGui Missing (Waited 10 Minutes)", "Invalid Client Data (r10002)",
+	".ROBLOXWALKSPEEDJUMPPOWER failure. Please rejoin.\nIncident ticket: 0x4F5A3C4", "ACLI: Loading Error [Took Too Long (>30 seconds)]",
+	"Loading Error: PlayerGui Missing (Waited Too Long)", "Invalid Client Data (r10002)",
 	"Communication Key Error (r10003)", "Invalid Remote Data (r10004)",
 	"Error. Client not firing remote.", "System Auth incorrect key",
 	"Invalid remote key generation.", "Remote key invalid.",
@@ -121,7 +137,7 @@ local function shadowBan(plr)
 		end
 
 		task.wait(math.random(25, 99))
-		plr:Kick(math.random(1, 4) == 3 and "Client Not Responding [Client hasn't checked in >5 minutes]" or shadowBanMessages[math.random(1, #shadowBanMessages)])
+		plr:Kick(math.random(1, 4) == 3 and "Client Not Responding [Client hasn't checked in >30 seconds]" or shadowBanMessages[math.random(1, #shadowBanMessages)])
 	end)()
 	if plr.Character then
 		coroutine.wrap(onShadowBanChar)(plr.Character)
@@ -133,14 +149,21 @@ Players.PlayerAdded:Connect(function(plr)
 	if (table.find(globalBans, plr.UserId) or table.find(serverBanTable, plr.UserId)) and table.find(unbannables, plr.UserId) == nil then
 		plr:Kick("You are banned from the game!")
 	elseif timedBans[tostring(plr.UserId)] ~= nil and table.find(unbannables, plr.UserId) == nil then
-		for k, v in pairs(timedBans) do
-			print(k, v)
-		end
 		local banData = string.split(timedBans[tostring(plr.UserId)], ";")
 		local timeLeft = os.time() - tonumber(banData[1])
 		local banLength = tonumber(banData[2]) 
 		if banLength - timeLeft >= 1 then
-			plr:Kick(tostring(banLength - timeLeft) .. " seconds left on ban.")
+			local kickMessage
+			if banLength - timeLeft <= 59 then
+				kickMessage = tostring(banLength - timeLeft) .. " seconds left on ban."
+			elseif banLength - timeLeft >= 60 and banLength - timeLeft <= 3599 then
+				kickMessage = tostring((banLength - timeLeft) / 60) .. " minutes left on ban."
+			elseif banLength - timeLeft >= 3600 and banLength - timeLeft <= 85999 then
+				kickMessage = tostring((banLength - timeLeft) / 3600) .. " hours left on ban."
+			else
+				kickMessage = tostring((banLength - timeLeft) / 86400) .. " days left on ban."
+			end
+			plr:Kick(kickMessage)
 		else
 			timedBans[tostring(plr.UserId)] = nil
 			saveTimeData()
@@ -159,7 +182,13 @@ Players.PlayerAdded:Connect(function(plr)
 	else
 		for i, v in ipairs(groupBans) do
 			if plr:IsInGroup(v) then
-				plr:Kick("You are banned from the game!")
+				plr:Kick("You have been banned from the game because you are in the group with the id: " .. v)
+			end
+		end
+		for i, v in ipairs(rankBans) do
+			v = string.split(v, ";")
+			if plr:GetRoleInGroup(v[1]) == v[2] then
+				plr:Kick("You are banned from the game because you are in the group with the id: " .. v .. " with a rank of " .. v[2])
 			end
 		end
 		print("No data found")
@@ -168,7 +197,7 @@ end)
 
 xpcall(function()
 	return MessagingService:SubscribeAsync("Ban", function(message)
-		local dataTable = string.split(message.Data, "â")
+		local dataTable = string.split(message.Data, "â___")
 		if Players:FindFirstChild(dataTable[1]) then
 			Players:FindFirstChild(dataTable[1]):Kick(dataTable[2] or "You have been banned from the game.")
 		end
@@ -190,10 +219,23 @@ end)
 
 xpcall(function()
 	return MessagingService:SubscribeAsync("GroupBan", function(message)
-		local dataTable = string.split(message.Data, "â")
+		local dataTable = string.split(message.Data, "â___")
 		for i, v in ipairs(Players:GetChildren()) do
 			if v:IsInGroup(tonumber(dataTable[1])) then
 				v:Kick(dataTable[2] or "You have been banned from the game because you are in the group with the id: " .. dataTable[1])
+			end
+		end
+	end)
+end, function(err)
+	warn("BanIt | Subscribing to ban list failed! Error: " .. err)
+end)
+
+xpcall(function()
+	return MessagingService:SubscribeAsync("RankBan", function(message)
+		local dataTable = string.split(message.Data, "â___")
+		for i, v in ipairs(Players:GetChildren()) do
+			if v:IsInGroup(tonumber(dataTable[1])) then
+				v:Kick(dataTable[3] or "You have been banned from the game because you are in the group with the id: " .. dataTable[1] .. " with the rank: " .. dataTable[2])
 			end
 		end
 	end)
@@ -211,7 +253,7 @@ function BanIt.ServerBan(plrUser, reason)
 		-- Code below gets the user ID by searching for their username then kicks them
 		local plr = assert(Players:GetUserIdFromNameAsync(plrUser), "No player found in database!")
 		if table.find(unbannables, plr) ~= nil then return end
-		table.insert(serverBanTable, plr)
+		table.insert(serverBanTable, plr .. ";" .. reason)
 		local potentialPlr = Players:FindFirstChild(plrUser)
 		if potentialPlr then
 			potentialPlr:Kick(reason or "You are banned from the server!")
@@ -234,7 +276,7 @@ function BanIt.Ban(plrUser, reason)
 			potentialPlr:Kick(reason or "You are banned from the game!")
 		else
 			xpcall(function()
-				return MessagingService:PublishAsync("Ban", plrUser .. "â" .. reason)
+				return MessagingService:PublishAsync("Ban", plrUser .. "â___" .. reason)
 			end, function(err)
 				warn("Ban data failed to publish. Error: " .. err)
 			end)
@@ -284,11 +326,19 @@ function BanIt.TimedBan(plrUser, num, numType)
 		timedBans[plr] = current .. ";" .. num
 		print(timedBans[plr])
 		saveTimeData()
+		local kickMessage
 		if Players:FindFirstChild(plrUser) then
-			Players[plrUser]:Kick("Banned for " .. num .. " " .. numType .. " from the game.")
+			if numType:lower() == "minutes" then
+				kickMessage = "Banned for " .. num / 60 .. " minutes from the game."
+			elseif numType:lower() == "hours" then
+				kickMessage = "Banned for " .. num / 3600 .. " hours from the game."
+			elseif numType:lower() == "days" then
+				kickMessage = "Banned for " .. num / 86400 .. " days from the game."
+			end
+			Players[plrUser]:Kick(kickMessage)
 		else
 			xpcall(function()
-				MessagingService:PublishAsync("Ban", plrUser .. "â" .. "Banned for " .. num .. " " .. numType .. " from the game.")
+				MessagingService:PublishAsync("Ban", kickMessage)
 			end, function(err)
 				warn("Error publishing ban data. Error: " .. err)
 			end)
@@ -296,86 +346,93 @@ function BanIt.TimedBan(plrUser, num, numType)
 	end, function(err)
 		warn("Error: " .. err)
 	end)
+end
+
+function BanIt.TimedUnban(plrUser)
+	local plr = assert(Players:GetUserIdFromNameAsync(plrUser), "No player found in database!")
+	timedBans[tostring(plr)] = nil
+	saveTimeData()
 end
 
 function BanIt.ShadowBan(plrUser)
-	xpcall(function()
-		-- Code finds first child (localplayer) below gets the user ID by searching for their username then kicks them with a false error
-		local plr = Players:FindFirstChild(plrUser)
-		if table.find(unbannables, plr) ~= nil then return end
-		local plrId = assert(Players:GetUserIdFromNameAsync(plrUser), "Not found a player with that name")
-		table.insert(shadowBans, plrId)
-		saveShadowBanData()
-		if plr then
-			shadowBan(plr)
-		else
-			xpcall(function()
-				MessagingService:PublishAsync("ShadowBan", plrUser)
-			end, function(err)
-				warn("Error publishing ban data. Error: " .. err)
-			end)
-		end
-	end, warn)
+	-- Code finds first child (localplayer) below gets the user ID by searching for their username then kicks them with a false error
+	local plr = Players:FindFirstChild(plrUser)
+	if table.find(unbannables, plr) ~= nil then return end
+	local plrId = assert(Players:GetUserIdFromNameAsync(plrUser), "No player found in database!")
+	table.insert(shadowBans, plrId)
+	saveShadowBanData()
+	if plr then
+		shadowBan(plr)
+	else
+		xpcall(function()
+			MessagingService:PublishAsync("ShadowBan", plrUser)
+		end, function(err)
+			warn("Error publishing ban data. Error: " .. err)
+		end)
+	end
 end
 
 function BanIt.ShadowUnban(plrUser)
-	xpcall(function()
-		-- Code below gets the user ID by searching for their username then removes the user from the shadowban datastore
-		local plr = assert(Players:GetUserIdFromNameAsync(plrUser), "Not found a player with that name")
-		local pos = table.find(shadowBans, plr)
-		table.remove(shadowBans, pos)
-		saveShadowBanData()
-	end, warn)
+	-- Code below gets the user ID by searching for their username then removes the user from the shadowban datastore
+	local plr = assert(Players:GetUserIdFromNameAsync(plrUser), "No player found in database!")
+	local pos = table.find(shadowBans, plr)
+	table.remove(shadowBans, pos)
+	saveShadowBanData()
 end
 
 function BanIt.SetUnbannable(plrUser)
-	xpcall(function()
-		local plr = assert(Players:GetUserIdFromNameAsync(plrUser), "No player found in database!")
-		table.insert(unbannables, plr)
-		saveUnbannableData()
-	end, function(err)
-		warn("Error: " .. err)
-	end)
+	local plr = assert(Players:GetUserIdFromNameAsync(plrUser), "No player found in database!")
+	table.insert(unbannables, plr)
+	saveUnbannableData()
 end
 
 function BanIt.SetBannable(plrUser)
-	xpcall(function()
-		local plr = assert(Players:GetUserIdFromNameAsync(plrUser), "No player found in database!")
-		if table.find(unbannables, plr) == nil then return end
-		table.remove(unbannables, table.find(unbannables, plr))
-		saveUnbannableData()
-	end, function(err)
-		warn("Error: " .. err)
-	end)
+	local plr = assert(Players:GetUserIdFromNameAsync(plrUser), "No player found in database!")
+	if table.find(unbannables, plr) == nil then return end
+	table.remove(unbannables, table.find(unbannables, plr))
+	saveUnbannableData()
 end
 
 function BanIt.GroupBan(groupId, reason)
-	xpcall(function()
-		table.insert(groupBans, groupId)
-		saveGroupData()
-		for i, v in ipairs(Players:GetChildren()) do
-			if v:IsInGroup(groupId) then
-				v:Kick(reason or "You have been banned from the game because you are in the group with the id: " .. tostring(groupId))
-			else
-				xpcall(function()
-					return MessagingService:PublishAsync("GroupBan", tostring(groupId) .. "â" .. reason)
-				end, function(err)
-					warn("Ban data failed to publish. Error: " .. err)
-				end)
-			end
+	table.insert(groupBans, groupId)
+	saveGroupData()
+	for i, v in ipairs(Players:GetChildren()) do
+		if v:IsInGroup(groupId) then
+			v:Kick(reason or "You have been banned from the game because you are in the group with the id: " .. tostring(groupId))
+		else
+			xpcall(function()
+				return MessagingService:PublishAsync("GroupBan", tostring(groupId) .. "â___" .. reason)
+			end, function(err)
+				warn("Ban data failed to publish. Error: " .. err)
+			end)
 		end
-	end, function(err)
-		warn("Error: " .. err)
-	end)
+	end
 end
 
 function BanIt.GroupUnban(groupId)
-	xpcall(function()
-		table.remove(groupBans, table.find(groupBans, groupId))
-		saveGroupData()
-	end, function(err)
-		warn("Error: " .. err)
-	end)
+	table.remove(groupBans, table.find(groupBans, groupId))
+	saveGroupData()
+end
+
+function BanIt.RankBan(groupId, roleName, reason)
+	table.insert(groupBans, groupId .. ";" .. roleName)
+	saveRankData()
+	for i, v in ipairs(Players:GetChildren()) do
+		if v:GetRoleInGroup(groupId) == roleName then
+			v:Kick(reason or "You have been banned from the game because you are in the group with the id: " .. tostring(groupId) .. " with the rank: " .. roleName)
+		else
+			xpcall(function()
+				return MessagingService:PublishAsync("RankBan", tostring(groupId) .. "â___" .. roleName .. "â___" .. reason)
+			end, function(err)
+				warn("Ban data failed to publish. Error: " .. err)
+			end)
+		end
+	end
+end
+
+function BanIt.RankUnban(groupId, roleName)
+	table.remove(rankBans, table.find(groupBans, groupId .. ";" .. roleName))
+	saveRankData()
 end
 
 return BanIt
